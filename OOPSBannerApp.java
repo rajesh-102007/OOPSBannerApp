a798851638e113bc261789170a8af832b60ad762
rajesh-102007 <ra2735@srmist.edu.in>
2026-02-16 13:33:01 +0530

    Added Program 2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,7 @@
-/**
- * @author [Your Name]
- * @version 1.0
- */
-public class OOPSBannerApp {
-
-    public static void main(String[] args) {
-        // Now we just call the method instead of writing all the print lines here
-        displayOopsBanner();
-    }
-
-    // This is the new function (method) we created to hold the banner logic
-    public static void displayOopsBanner() {
-        System.out.println(" ****   ****  ****  **** ");
-        System.out.println("*    * *    * *   * * ");
-        System.out.println("*    * *    * ****  **** ");
-        System.out.println("*    * *    * *        *");
-        System.out.println(" ****   ****  *     **** ");
+class PrintOOPS
+{
+    public static void main()
+    {
+        System.out.println("OOPS");
     }
 }
