935fbb2f183b90e8910b3fd460726fd65c2bfff0
rajesh-102007 <ra2735@srmist.edu.in>
2026-02-23 13:13:58 +0530

    Added Program 5

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,21 +5,15 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        // Now we just call the method instead of writing all the print lines here
-        displayOopsBanner();
-    }
-
-    // This is the new function (method) we created to hold the banner logic
-    public static void displayOopsBanner() {
-        String[] lines = new String[7];
-        lines[0] = String.join(" ","  ***   ","  ***  ","******","    ***** ");
-        lines[1] = String.join(" ","**    **","**   **","*    *"," **      ");
-        lines[2] = String.join(" ","**    **","**   **","*    *","**       ");
-        lines[3] = String.join(" ","**    **","**   **","****** ","   *****  ");
-        lines[4] = String.join(" ","**    **","**   **","**    ","         ** ");
-        lines[5] = String.join(" ","**    **","**   **","**    "," **      ** ");
-        lines[6] = String.join(" ","  ***   ","  ***  ","**    ","    *****      ");
-
+        String[] lines = {
+            String.join(" ","  ***   ","  ***  ","******","    ***** "),
+            String.join(" ","**    **","**   **","*    *"," **      "),
+            String.join(" ","**    **","**   **","*    *","**       "),
+            String.join(" ","**    **","**   **","****** ","   *****  "),
+            String.join(" ","**    **","**   **","**    ","         ** "),
+            String.join(" ","**    **","**   **","**    "," **      ** "),
+            String.join(" ","  ***   ","  ***  ","**    ","    *****      ")
+        };
    for (String line : lines) {
     System.out.println(line);
    }
